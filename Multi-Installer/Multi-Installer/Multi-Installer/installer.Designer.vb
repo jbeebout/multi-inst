6a14625cf5e28bc8f8e9ee03de2f9fc5c19d0c21
JBeebout <53784503+jbeebout@users.noreply.github.com>
2022-04-05 05:27:15 -0400

    Add files via upload

---
+++ Multi-Installer/Multi-Installer/Multi-Installer/installer.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class installer
+    Inherits System.Windows.Forms.Form
+
+
+
+
+
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(installer))
+        Me.libre = New System.Windows.Forms.CheckBox()
+        Me.ghostscript = New System.Windows.Forms.CheckBox()
+        Me.reader = New System.Windows.Forms.CheckBox()
+        Me.pdffilter = New System.Windows.Forms.CheckBox()
+        Me.zan = New System.Windows.Forms.CheckBox()
+        Me.begin = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'libre
+        '
+        Me.libre.AutoSize = True
+        Me.libre.Location = New System.Drawing.Point(56, 52)
+        Me.libre.Name = "libre"
+        Me.libre.Size = New System.Drawing.Size(77, 17)
+        Me.libre.TabIndex = 0
+        Me.libre.Text = "LibreOffice"
+        Me.libre.UseVisualStyleBackColor = True
+        '
+        'ghostscript
+        '
+        Me.ghostscript.AutoSize = True
+        Me.ghostscript.Location = New System.Drawing.Point(56, 94)
+        Me.ghostscript.Name = "ghostscript"
+        Me.ghostscript.Size = New System.Drawing.Size(81, 17)
+        Me.ghostscript.TabIndex = 1
+        Me.ghostscript.Text = "GhostScript"
+        Me.ghostscript.UseVisualStyleBackColor = True
+        '
+        'reader
+        '
+        Me.reader.AutoSize = True
+        Me.reader.Location = New System.Drawing.Point(56, 132)
+        Me.reader.Name = "reader"
+        Me.reader.Size = New System.Drawing.Size(92, 17)
+        Me.reader.TabIndex = 2
+        Me.reader.Text = "AdobeReader"
+        Me.reader.UseVisualStyleBackColor = True
+        '
+        'pdffilter
+        '
+        Me.pdffilter.AutoSize = True
+        Me.pdffilter.Location = New System.Drawing.Point(56, 170)
+        Me.pdffilter.Name = "pdffilter"
+        Me.pdffilter.Size = New System.Drawing.Size(72, 17)
+        Me.pdffilter.TabIndex = 3
+        Me.pdffilter.Text = "PDF Filter"
+        Me.pdffilter.UseVisualStyleBackColor = True
+        '
+        'zan
+        '
+        Me.zan.AutoSize = True
+        Me.zan.Location = New System.Drawing.Point(56, 203)
+        Me.zan.Name = "zan"
+        Me.zan.Size = New System.Drawing.Size(107, 17)
+        Me.zan.TabIndex = 4
+        Me.zan.Text = "Zan ImagePrinter"
+        Me.zan.UseVisualStyleBackColor = True
+        '
+        'begin
+        '
+        Me.begin.Location = New System.Drawing.Point(56, 297)
+        Me.begin.Name = "begin"
+        Me.begin.Size = New System.Drawing.Size(75, 23)
+        Me.begin.TabIndex = 5
+        Me.begin.Text = "Begin"
+        Me.begin.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.begin.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(356, 52)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(417, 168)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'installer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.begin)
+        Me.Controls.Add(Me.zan)
+        Me.Controls.Add(Me.pdffilter)
+        Me.Controls.Add(Me.reader)
+        Me.Controls.Add(Me.ghostscript)
+        Me.Controls.Add(Me.libre)
+        Me.Name = "installer"
+        Me.Text = "Multi-Installer"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents libre As Windows.Forms.CheckBox
+    Friend WithEvents ghostscript As Windows.Forms.CheckBox
+    Friend WithEvents reader As Windows.Forms.CheckBox
+    Friend WithEvents pdffilter As Windows.Forms.CheckBox
+    Friend WithEvents zan As Windows.Forms.CheckBox
+    Friend WithEvents begin As Windows.Forms.Button
+    Friend WithEvents PictureBox1 As Windows.Forms.PictureBox
+End Class
---
+++ Multi-Installer/Multi-Installer/Multi-Installer/installer.vb
@@ -0,0 +1,186 @@
+﻿Imports System.Diagnostics
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.Win32
+
+Public Class installer
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles libre.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(sender As Object, e As EventArgs) Handles ghostscript.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_2(sender As Object, e As EventArgs) Handles pdffilter.CheckedChanged
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles Me.Load
+        set_checks_for_reg()
+    End Sub
+
+
+    Private Sub begin_Click(sender As Object, e As EventArgs) Handles begin.Click
+
+
+
+        start_and_wait_libre()
+
+
+
+
+    End Sub
+
+    Private Sub set_checks_for_reg()
+        Dim regVersion As Microsoft.Win32.RegistryKey
+        Dim keyValue = "SOFTWARE\LibreOffice\LibreOffice\6.4\"
+        regVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(keyValue, False)
+
+        Dim TS__x64 As Microsoft.Win32.RegistryKey = Microsoft.Win32.RegistryKey.OpenBaseKey _
+                                            (Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry64)
+        Try
+            Dim libre_path As String = TS__x64.OpenSubKey("SOFTWARE\LibreOffice\LibreOffice\6.4").GetValue("Path")
+            If libre_path IsNot Nothing Then
+                libre.CheckState = 1
+            End If
+        Catch
+        End Try
+
+        Try
+            Dim gs_path As String = TS__x64.OpenSubKey("SOFTWARE\GPL Ghostscript\9.23").GetValue("GS_DLL")
+            If gs_path IsNot Nothing Then
+                ghostscript.CheckState = 1
+            End If
+        Catch
+        End Try
+
+        'check for reader
+        If My.Computer.FileSystem.DirectoryExists("C:\Program Files (x86)\Adobe\Acrobat Reader DC") Then
+            reader.CheckState = 1
+
+        End If
+
+        'check for zan
+        If My.Computer.FileSystem.DirectoryExists("C:\Program Files\zvprt50") Then
+            zan.CheckState = 1
+
+        End If
+
+        'here we check a directory, its fine to just check one location as its a x64 plugin
+        If My.Computer.FileSystem.DirectoryExists("C:\Program Files\Adobe\Adobe PDF iFilter 11 for 64-bit platforms") Then
+            pdffilter.CheckState = 1
+
+        End If
+
+    End Sub
+    Private Sub start_and_wait_reader()
+        If reader.CheckState <> Windows.Forms.CheckState.Checked Then
+            Dim reader_running As New System.Diagnostics.Process()
+            Try
+                reader_running = Process.Start("readerdc_en_xa_crd_install.exe")
+            Catch ex As Exception
+                Dim response = MsgBox("Adobe Reader installer did not start", MsgBoxStyle.Critical, " Error Msg: ")
+            End Try
+            Do Until reader_running.HasExited = True
+            Loop
+
+
+            reader.CheckState = 1
+            start_and_wait_pdffilter()
+
+        Else
+            start_and_wait_pdffilter()
+        End If
+    End Sub
+
+    Private Sub start_and_wait_libre()
+        If libre.CheckState <> Windows.Forms.CheckState.Checked Then
+            Dim libre_running As New System.Diagnostics.Process()
+            Try
+                libre_running = Process.Start("LibreOffice_6.4.5_Win_x64.msi")
+            Catch ex As Exception
+                Dim response = MsgBox("LibreOffice installer did not start", MsgBoxStyle.Critical, " Error Msg: ")
+            End Try
+            Do Until libre_running.HasExited = True
+            Loop
+
+
+            libre.CheckState = 1
+            start_and_wait_ghostscript()
+
+        Else
+            start_and_wait_ghostscript()
+        End If
+    End Sub
+    Private Sub start_and_wait_ghostscript()
+        If ghostscript.CheckState <> Windows.Forms.CheckState.Checked Then
+            Dim gs_running As New System.Diagnostics.Process()
+            Try
+                gs_running = Process.Start("gs923w64.exe")
+            Catch ex As Exception
+                Dim response = MsgBox("GhostScript installer did not start", MsgBoxStyle.Critical, " Error Msg: ")
+            End Try
+            Do Until gs_running.HasExited = True
+            Loop
+
+
+            ghostscript.CheckState = 1
+            start_and_wait_reader()
+
+        Else
+            start_and_wait_reader()
+        End If
+
+    End Sub
+    Private Sub start_and_wait_pdffilter()
+        If pdffilter.CheckState <> Windows.Forms.CheckState.Checked Then
+            Dim filter_running As New System.Diagnostics.Process()
+            Try
+                filter_running = Process.Start("PDFFilter64Setup.msi")
+            Catch ex As Exception
+                Dim response = MsgBox("PDF-Filter installer did not start", MsgBoxStyle.Critical, " Error Msg: ")
+            End Try
+            Do Until filter_running.HasExited = True
+            Loop
+
+            pdffilter.CheckState = 1
+            start_and_wait_zan()
+
+        Else
+            start_and_wait_zan()
+        End If
+
+    End Sub
+    Private Sub start_and_wait_zan()
+        If zan.CheckState <> Windows.Forms.CheckState.Checked Then
+            Dim zan_running As New System.Diagnostics.Process()
+            Try
+                zan_running = Process.Start("/ZanforDataEngineer/zvprt5_setup_x64.exe")
+            Catch ex As Exception
+                Dim response = MsgBox("Zan installer did not start", MsgBoxStyle.Critical, " Error Msg: ")
+            End Try
+            Do Until zan_running.HasExited = True
+            Loop
+
+            zan.CheckState = 1
+            install_finished()
+
+        Else
+            install_finished()
+        End If
+
+    End Sub
+    Private Sub install_finished()
+        Dim response = MsgBox("Installation Complete", MsgBoxStyle.OkOnly, " Finished! ")
+    End Sub
+
+    Private Sub reader_CheckedChanged(sender As Object, e As EventArgs) Handles reader.CheckedChanged
+
+    End Sub
+
+    Private Sub zan_CheckedChanged(sender As Object, e As EventArgs) Handles zan.CheckedChanged
+
+    End Sub
+End Class
+
